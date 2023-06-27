2c058028a15f6d41d1abce8c8a5166f6337214b3
Jean Silva <jeanbarrossilva@outlook.com>
2023-06-27 20:58:01 -0300

    style: break limit-exceeding line at Follow.toHeaderFollow

--- feature/profile-details/src/main/java/com/jeanbarrossilva/mastodon/feature/profiledetails/Follow.extensions.kt
+++ feature/profile-details/src/main/java/com/jeanbarrossilva/mastodon/feature/profiledetails/Follow.extensions.kt
@@ -5,7 +5,8 @@ import com.jeanbarrossilva.mastodonte.core.profile.follow.Follow
 /** Converts this [Follow] into a [ProfileDetails.Followable.Follow]. **/
 internal fun Follow.toHeaderFollow(): ProfileDetails.Followable.Follow {
     return when (this) {
-        Follow.Public.following(), Follow.Private.following() -> ProfileDetails.Followable.Follow.FOLLOWING
+        Follow.Public.following(), Follow.Private.following() ->
+            ProfileDetails.Followable.Follow.FOLLOWING
         Follow.Private.requested() -> ProfileDetails.Followable.Follow.REQUESTED
         else -> ProfileDetails.Followable.Follow.UNFOLLOWED
     }
