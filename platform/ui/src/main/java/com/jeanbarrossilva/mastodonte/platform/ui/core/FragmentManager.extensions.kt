76f66a37b73ecf64281841c3f3eb7dc0557b2ad4
Jean Silva <jeanbarrossilva@outlook.com>
2023-06-30 14:58:55 -0300

    refact: replace FragmentManager.animate's isAnimated Boolean by a transition Int

--- platform/ui/src/main/java/com/jeanbarrossilva/mastodonte/platform/ui/core/FragmentManager.extensions.kt
+++ platform/ui/src/main/java/com/jeanbarrossilva/mastodonte/platform/ui/core/FragmentManager.extensions.kt
@@ -1,28 +1,44 @@
 package com.jeanbarrossilva.mastodonte.platform.ui.core
 
 import androidx.annotation.IdRes
+import androidx.annotation.IntDef
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
 
 /**
+ * Denotes that an [Int] value should refer to one of [FragmentTransaction]'s transition styles.
+ *
+ * @see FragmentTransaction.TRANSIT_NONE
+ * @see FragmentTransaction.TRANSIT_FRAGMENT_OPEN
+ * @see FragmentTransaction.TRANSIT_FRAGMENT_CLOSE
+ * @see FragmentTransaction.TRANSIT_FRAGMENT_FADE
+ **/
+@IntDef(
+    FragmentTransaction.TRANSIT_NONE,
+    FragmentTransaction.TRANSIT_FRAGMENT_OPEN,
+    FragmentTransaction.TRANSIT_FRAGMENT_CLOSE,
+    FragmentTransaction.TRANSIT_FRAGMENT_FADE
+)
+@Retention(AnnotationRetention.SOURCE)
+private annotation class Transit
+
+/**
  * Navigates to the given [destination] by adding it to the container.
  *
  * @param containerID ID of the [Fragment] container.
- * @param isAnimated Whether the transition is animated.
+ * @param transition Indicates how the transition will be animated.
  * @param destination Callback that provides the [Fragment] that'll get navigated to.
  **/
 fun FragmentManager.navigate(
     @IdRes containerID: Int,
-    isAnimated: Boolean = true,
+    @Transit transition: Int = FragmentTransaction.TRANSIT_FRAGMENT_OPEN,
     destination: () -> Fragment
 ) {
     commit {
         addToBackStack(null)
-        if (isAnimated) {
-            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-        }
+        setTransition(transition)
         add(containerID, destination())
     }
 }
