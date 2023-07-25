b0d5eea3958cfdd375665197ffa9ab2bdb075c1f
Jean Silva <jeanbarrossilva@outlook.com>
2023-07-25 06:53:18 -0300

    refact: parse String with Account.Companion.of at MastodonToot.toAuthor

--- core/mastodon/src/main/java/com/jeanbarrossilva/mastodonte/core/mastodon/account/MastodonAccount.kt
+++ core/mastodon/src/main/java/com/jeanbarrossilva/mastodonte/core/mastodon/account/MastodonAccount.kt
@@ -1,6 +1,6 @@
 package com.jeanbarrossilva.mastodonte.core.mastodon.account
 
-import com.jeanbarrossilva.mastodonte.core.account.at
+import com.jeanbarrossilva.mastodonte.core.account.Account
 import com.jeanbarrossilva.mastodonte.core.toot.Author
 import java.net.URL
 
@@ -13,9 +13,7 @@ internal data class MastodonAccount(
 ) {
     fun toAuthor(): Author {
         val avatarURL = URL(avatar)
-        val account = with(acct.split('@')) {
-            if (size == 2) first() at get(2) else first() at "mastodon.social"
-        }
+        val account = Account.of(acct, "mastodon.social")
         val profileURL = URL(url)
         return Author(id, avatarURL, displayName, account, profileURL)
     }
