bcbd55291a0228a690402a3415f3137147ff94cb
Jean Silva <jeanbarrossilva@outlook.com>
2023-08-01 07:34:27 -0300

    style: fix import ordering at navigation item navigators

--- app/src/main/java/com/jeanbarrossilva/orca/app/navigation/navigator/provider/FeedBottomNavigationItemNavigator.kt
+++ app/src/main/java/com/jeanbarrossilva/orca/app/navigation/navigator/provider/FeedBottomNavigationItemNavigator.kt
@@ -2,10 +2,10 @@ package com.jeanbarrossilva.orca.app.navigation.navigator.provider
 
 import androidx.fragment.app.Fragment
 import com.jeanbarrossilva.orca.app.R
+import com.jeanbarrossilva.orca.app.navigation.navigator.BottomNavigationItemNavigator
 import com.jeanbarrossilva.orca.core.feed.profile.Profile
 import com.jeanbarrossilva.orca.core.sample.feed.profile.sample
 import com.jeanbarrossilva.orca.feature.feed.FeedFragment
-import com.jeanbarrossilva.orca.app.navigation.navigator.BottomNavigationItemNavigator
 
 internal class FeedBottomNavigationItemNavigator(
     override val next: BottomNavigationItemNavigator?
--- app/src/main/java/com/jeanbarrossilva/orca/app/navigation/navigator/provider/ProfileBottomNavigationItemNavigator.kt
+++ app/src/main/java/com/jeanbarrossilva/orca/app/navigation/navigator/provider/ProfileBottomNavigationItemNavigator.kt
@@ -2,11 +2,11 @@ package com.jeanbarrossilva.orca.app.navigation.navigator.provider
 
 import androidx.fragment.app.Fragment
 import com.jeanbarrossilva.orca.app.R
+import com.jeanbarrossilva.orca.app.navigation.navigator.BottomNavigationItemNavigator
 import com.jeanbarrossilva.orca.core.feed.profile.Profile
 import com.jeanbarrossilva.orca.core.sample.feed.profile.sample
 import com.jeanbarrossilva.orca.feature.profiledetails.ProfileDetailsFragment
 import com.jeanbarrossilva.orca.feature.profiledetails.navigation.BackwardsNavigationState
-import com.jeanbarrossilva.orca.app.navigation.navigator.BottomNavigationItemNavigator
 
 internal class ProfileBottomNavigationItemNavigator(
     override val next: BottomNavigationItemNavigator?
