733564f17a5a2c9e6ee588a50f7fbb006c1d4129
Jean Silva <jeanbarrossilva@outlook.com>
2023-08-03 21:46:28 -0300

    refact: return pre-instantiated Flow on ProfileSearcher's search method

--- core/src/main/java/com/jeanbarrossilva/orca/core/feed/profile/search/ProfileSearcher.kt
+++ core/src/main/java/com/jeanbarrossilva/orca/core/feed/profile/search/ProfileSearcher.kt
@@ -1,27 +1,40 @@
 package com.jeanbarrossilva.orca.core.feed.profile.search
 
 import kotlin.time.Duration.Companion.milliseconds
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flowOf
 
 /** Searches for profiles through [search]. **/
 abstract class ProfileSearcher {
+    /** [MutableStateFlow] to which the query is emitted. **/
+    private val queryFlow = MutableStateFlow("")
+
+    /** [Flow] with an empty [List] of [ProfileSearchResult]s. **/
+    private val emptyResultsFlow = flowOf(emptyList<ProfileSearchResult>())
+
+    /**
+     * [MutableStateFlow] to which [ProfileSearchResult]s based on the query are emitted.
+     *
+     * @see queryFlow
+     **/
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    private val resultsFlow = queryFlow.debounce(256.milliseconds).flatMapMerge {
+        if (it.isBlank()) emptyResultsFlow else onSearch(it)
+    }
+
     /**
      * Searches for profiles that match the [query].
      *
      * @param query Input about the profiles being searched.
      **/
-    suspend fun search(query: String): Flow<List<ProfileSearchResult>> {
-        val formattedQuery = query.trim()
-
-        @OptIn(FlowPreview::class)
-        return if (formattedQuery.isBlank()) {
-            flowOf(emptyList())
-        } else {
-            onSearch(formattedQuery).debounce(500.milliseconds)
-        }
+    fun search(query: String): Flow<List<ProfileSearchResult>> {
+        queryFlow.value = query.trim()
+        return resultsFlow
     }
 
     /**
