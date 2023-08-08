098c34c3f44a043ee305295d170abe80239348b2
Jean Silva <jeanbarrossilva@outlook.com>
2023-08-08 14:56:14 -0300

    fix(test:ProfileDetailsFragment): missing OnBottomAreaAvailabilityChangeListener injection

--- feature/profile-details/src/test/java/com/jeanbarrossilva/orca/feature/profiledetails/test/ProfileDetailsModule.kt
+++ feature/profile-details/src/test/java/com/jeanbarrossilva/orca/feature/profiledetails/test/ProfileDetailsModule.kt
@@ -5,6 +5,7 @@ import com.jeanbarrossilva.orca.core.feed.profile.toot.TootProvider
 import com.jeanbarrossilva.orca.core.sample.feed.profile.SampleProfileProvider
 import com.jeanbarrossilva.orca.core.sample.feed.profile.toot.SampleTootProvider
 import com.jeanbarrossilva.orca.feature.profiledetails.ProfileDetailsBoundary
+import com.jeanbarrossilva.orca.platform.theme.reactivity.OnBottomAreaAvailabilityChangeListener
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -14,5 +15,6 @@ internal fun ProfileDetailsModule(): Module {
         single<ProfileProvider> { SampleProfileProvider }
         single<TootProvider> { SampleTootProvider }
         single<ProfileDetailsBoundary> { TestProfileDetailsBoundary() }
+        single { OnBottomAreaAvailabilityChangeListener.empty }
     }
 }
