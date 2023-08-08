3ec2efaf6e9846ae48b120abd694fde478557b92
Jean Silva <jeanbarrossilva@outlook.com>
2023-08-08 15:56:16 -0300

    style: remove unused imports and line breaks on typealias definitions

--- core/mastodon/src/main/java/com/jeanbarrossilva/orca/core/mastodon/auth/authorization/ui/AuthorizationActivity.kt
+++ core/mastodon/src/main/java/com/jeanbarrossilva/orca/core/mastodon/auth/authorization/ui/AuthorizationActivity.kt
@@ -1,7 +1,6 @@
 package com.jeanbarrossilva.orca.core.mastodon.auth.authorization.ui
 
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
