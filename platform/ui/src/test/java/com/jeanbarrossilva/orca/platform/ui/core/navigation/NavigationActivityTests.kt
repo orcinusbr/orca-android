7aa05b9f384da3e9894dd2600cebe8536eba525a
Jean Silva <jeanbarrossilva@outlook.com>
2023-08-15 07:08:04 -0300

    test: NavigationActivity's FragmentContainerView ID assignment

--- platform/ui/src/test/java/com/jeanbarrossilva/orca/platform/ui/core/navigation/NavigationActivityTests.kt
+++ platform/ui/src/test/java/com/jeanbarrossilva/orca/platform/ui/core/navigation/NavigationActivityTests.kt
@@ -1,8 +1,10 @@
 package com.jeanbarrossilva.orca.platform.ui.core.navigation
 
 import android.os.Bundle
+import android.view.View
 import android.widget.FrameLayout
 import androidx.fragment.app.FragmentContainerView
+import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertThrows
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,10 +15,18 @@ import org.robolectric.RobolectricTestRunner
 internal class NavigationActivityTests {
     class NoFragmentContainerViewActivity : NavigationActivity()
 
-    class FragmentContainerViewActivity : NavigationActivity() {
+    class UnidentifiedFragmentContainerViewActivity : NavigationActivity() {
+        var view: FragmentContainerView? = null
+
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            setContentView(FragmentContainerView(this))
+            view = FragmentContainerView(this)
+            setContentView(view)
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            view = null
         }
     }
 
@@ -60,9 +70,21 @@ internal class NavigationActivityTests {
     }
 
     @Test
+    fun `GIVEN an Activity with an unidentified FragmentContainerView WHEN getting the navigator THEN it's identified`() { // ktlint-disable max-line-length
+        Robolectric
+            .buildActivity(UnidentifiedFragmentContainerViewActivity::class.java)
+            .setup()
+            .use {
+                it.get().navigator
+                assertNotEquals(View.NO_ID, it.get().view?.id)
+            }
+    }
+
+    @Test
     fun `GIVEN an Activity with a FragmentContainerView WHEN getting the navigator THEN it's obtained`() { // ktlint-disable max-line-length
-        Robolectric.buildActivity(FragmentContainerViewActivity::class.java).setup().use {
-            it.get().navigator
-        }
+        Robolectric
+            .buildActivity(UnidentifiedFragmentContainerViewActivity::class.java)
+            .setup()
+            .use { it.get().navigator }
     }
 }
