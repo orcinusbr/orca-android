56a512991256756cab3caf5803ff0c21bdd20f2d
Jean Silva <jeanbarrossilva@outlook.com>
2023-09-06 08:49:24 -0300

    config: run :feature:feed's tests on the emulator

--- feature/feed/src/androidTest/java/com/jeanbarrossilva/orca/feature/feed/FeedFragmentTests.kt
+++ feature/feed/src/androidTest/java/com/jeanbarrossilva/orca/feature/feed/FeedFragmentTests.kt
@@ -7,42 +7,40 @@ import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.compose.ui.test.onChildren
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.performClick
+import androidx.test.core.app.launchActivity
 import com.jeanbarrossilva.orca.core.feed.profile.Profile
 import com.jeanbarrossilva.orca.core.sample.feed.profile.sample
 import com.jeanbarrossilva.orca.core.sample.feed.profile.toot.SampleTootWriter
 import com.jeanbarrossilva.orca.feature.feed.test.FeedActivity
 import com.jeanbarrossilva.orca.platform.ui.component.timeline.toot.TOOT_PREVIEW_FAVORITE_STAT_TAG
 import com.jeanbarrossilva.orca.platform.ui.test.component.timeline.toot.onTootPreviews
+import com.jeanbarrossilva.orca.platform.ui.test.component.timeline.toot.time.Time4JTestRule
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
 internal class FeedFragmentTests {
+    @get:Rule
+    val time4JRule = Time4JTestRule()
+
     @get:Rule
     val composeRule = createEmptyComposeRule()
 
     @Test
-    fun `GIVEN a toot WHEN marking it as favorite THEN it's favorited`() {
+    fun favoritesToot() {
         runTest {
             SampleTootWriter
                 .updateFavorite(Profile.sample.getToots(page = 0).first().first().id, false)
         }
-        Robolectric
-            .buildActivity(FeedActivity::class.java, FeedActivity.getIntent(Profile.sample.id))
-            .setup()
-            .use {
-                composeRule
-                    .onTootPreviews()
-                    .onFirst()
-                    .onChildren()
-                    .filterToOne(hasTestTag(TOOT_PREVIEW_FAVORITE_STAT_TAG))
-                    .performClick()
-                    .assertIsSelected()
-            }
+        launchActivity<FeedActivity>(FeedActivity.getIntent(Profile.sample.id)).use {
+            composeRule
+                .onTootPreviews()
+                .onFirst()
+                .onChildren()
+                .filterToOne(hasTestTag(TOOT_PREVIEW_FAVORITE_STAT_TAG))
+                .performClick()
+                .assertIsSelected()
+        }
     }
 }
--- feature/feed/src/androidTest/java/com/jeanbarrossilva/orca/feature/feed/FeedTests.kt
+++ feature/feed/src/androidTest/java/com/jeanbarrossilva/orca/feature/feed/FeedTests.kt
@@ -12,25 +12,26 @@ import com.jeanbarrossilva.orca.platform.theme.OrcaTheme
 import com.jeanbarrossilva.orca.platform.ui.component.timeline.toot.TOOT_PREVIEW_FAVORITE_STAT_TAG
 import com.jeanbarrossilva.orca.platform.ui.component.timeline.toot.TootPreview
 import com.jeanbarrossilva.orca.platform.ui.test.component.timeline.toot.onTootPreviews
+import com.jeanbarrossilva.orca.platform.ui.test.component.timeline.toot.time.Time4JTestRule
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
 internal class FeedTests {
+    @get:Rule
+    val time4JRule = Time4JTestRule()
+
     @get:Rule
     val composeRule = createComposeRule()
 
     @Test
-    fun `GIVEN a toot's favorite stat WHEN clicking it THEN the callback is called`() {
-        var hasCallbackBeenCalled = false
+    fun runsCallbackWhenClickingTootFavoriteStat() {
+        var hasCallbackBeenRun = false
         composeRule.setContent {
             OrcaTheme {
                 Feed(
                     TootPreview.samples.toSerializableList().toListLoadable(),
-                    onFavorite = { hasCallbackBeenCalled = true }
+                    onFavorite = { hasCallbackBeenRun = true }
                 )
             }
         }
@@ -40,6 +41,6 @@ internal class FeedTests {
             .onChildren()
             .filterToOne(hasTestTag(TOOT_PREVIEW_FAVORITE_STAT_TAG))
             .performClick()
-        assertTrue(hasCallbackBeenCalled)
+        assertTrue(hasCallbackBeenRun)
     }
 }
