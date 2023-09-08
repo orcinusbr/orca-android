6ab0feb41175c802f4077cd043906ad337cdb3ac
Jean Silva <jeanbarrossilva@outlook.com>
2023-09-08 14:15:57 -0300

    refact: add attachment URLs to toot content

---
+++ core/mastodon/src/main/java/com/jeanbarrossilva/orca/core/mastodon/feed/profile/toot/status/MastodonAttachment.kt
@@ -0,0 +1,16 @@
+package com.jeanbarrossilva.orca.core.mastodon.feed.profile.toot.status
+
+import com.jeanbarrossilva.orca.core.feed.profile.toot.content.Attachment
+import java.net.URL
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class MastodonAttachment(val previewUrl: String, val description: String) {
+    private val coreAttachment by lazy {
+        Attachment(description.ifBlank { null }, URL(previewUrl))
+    }
+
+    fun toAttachment(): Attachment {
+        return coreAttachment
+    }
+}
