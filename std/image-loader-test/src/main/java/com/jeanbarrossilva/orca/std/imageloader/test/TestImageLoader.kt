01f3811f4ea82a54ce16b08ea3cdd57c1b57ebc1
Jean Silva <jeanbarrossilva@outlook.com>
2023-09-09 18:00:04 -0300

    fix(build): TestImageLoader overriding nonexistent method

--- std/image-loader-test/src/main/java/com/jeanbarrossilva/orca/std/imageloader/test/TestImageLoader.kt
+++ std/image-loader-test/src/main/java/com/jeanbarrossilva/orca/std/imageloader/test/TestImageLoader.kt
@@ -6,8 +6,8 @@ import com.jeanbarrossilva.orca.std.imageloader.buildImage
 import java.net.URL
 
 /** [ImageLoader] that loads an empty [Image]. **/
-object TestImageLoader : ImageLoader() {
-    override suspend fun onLoad(width: Int, height: Int, url: URL): Image {
+object TestImageLoader : ImageLoader {
+    override suspend fun load(width: Int, height: Int, url: URL): Image {
         return buildImage(width = 1, height = 1) {
             pixel(0)
         }
