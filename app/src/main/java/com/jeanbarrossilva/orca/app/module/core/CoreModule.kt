0130300c40fd20856b1434b96eef2e619936c6fb
Jean Silva <jeanbarrossilva@outlook.com>
2023-09-29 07:48:56 -0300

    refact: make CoreModule extend Module

--- app/src/main/java/com/jeanbarrossilva/orca/app/module/core/CoreModule.kt
+++ app/src/main/java/com/jeanbarrossilva/orca/app/module/core/CoreModule.kt
@@ -3,14 +3,14 @@ package com.jeanbarrossilva.orca.app.module.core
 import com.jeanbarrossilva.orca.core.auth.SomeAuthenticationLock
 import com.jeanbarrossilva.orca.core.instance.InstanceProvider
 import com.jeanbarrossilva.orca.std.injector.Injectable
-import com.jeanbarrossilva.orca.std.injector.Injector
+import com.jeanbarrossilva.orca.std.injector.module.Module
 
-internal abstract class CoreModule {
+internal abstract class CoreModule : Module() {
     protected abstract val authenticationLock: Injectable<SomeAuthenticationLock>
     protected abstract val instanceProvider: Injectable<InstanceProvider>
 
-    fun inject() {
-        Injector.inject(authenticationLock)
-        Injector.inject(instanceProvider)
+    override val dependencies: Scope.() -> Unit = {
+        inject(authenticationLock)
+        inject(instanceProvider)
     }
 }
--- app/src/main/java/com/jeanbarrossilva/orca/app/module/core/MainCoreModule.kt
+++ app/src/main/java/com/jeanbarrossilva/orca/app/module/core/MainCoreModule.kt
@@ -9,24 +9,18 @@ import com.jeanbarrossilva.orca.core.http.instance.HttpInstanceProvider
 import com.jeanbarrossilva.orca.core.instance.InstanceProvider
 import com.jeanbarrossilva.orca.core.sharedpreferences.actor.SharedPreferencesActorProvider
 import com.jeanbarrossilva.orca.std.injector.Injectable
-import com.jeanbarrossilva.orca.std.injector.Injector
 
 internal class MainCoreModule : CoreModule() {
-    private val authorizer by lazy { HttpAuthorizer(context = Injector.get()) }
-    private val actorProvider by lazy { SharedPreferencesActorProvider(context = Injector.get()) }
-    private val authenticator by lazy {
-        HttpAuthenticator(context = Injector.get(), authorizer, actorProvider)
+    override val dependencies: Scope.() -> Unit = {
+        inject { HttpAuthorizer(context = get()) }
+        inject<ActorProvider> { SharedPreferencesActorProvider(context = get()) }
+        inject { HttpAuthenticator(context = get(), authorizer = get(), actorProvider = get()) }
+        super.dependencies(this)
     }
 
     override val authenticationLock = Injectable<SomeAuthenticationLock> {
-        AuthenticationLock(authenticator, actorProvider = get())
+        AuthenticationLock(authenticator = get(), actorProvider = get())
     }
     override val instanceProvider =
         Injectable<InstanceProvider> { HttpInstanceProvider(context = get()) }
-
-    init {
-        Injector.inject(authorizer)
-        Injector.inject<ActorProvider>(actorProvider)
-        Injector.inject(authenticator)
-    }
 }
