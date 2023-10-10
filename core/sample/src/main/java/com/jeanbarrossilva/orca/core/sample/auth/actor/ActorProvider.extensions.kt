ef752e4965cdeb0b4c54c3e1271318d11e48d296
Jean Silva <jeanbarrossilva@outlook.com>
2023-10-10 00:11:15 -0300

    fix(build): unresolved ref to CoreModule in demo flavor dimension

--- core/sample/src/main/java/com/jeanbarrossilva/orca/core/sample/auth/actor/ActorProvider.extensions.kt
+++ core/sample/src/main/java/com/jeanbarrossilva/orca/core/sample/auth/actor/ActorProvider.extensions.kt
@@ -3,8 +3,8 @@ package com.jeanbarrossilva.orca.core.sample.auth.actor
 import com.jeanbarrossilva.orca.core.auth.actor.Actor
 import com.jeanbarrossilva.orca.core.auth.actor.ActorProvider
 
-/** [ActorProvider] that always provides a sample [authenticated][Actor.Authenticated] [Actor]. **/
-internal object SampleActorProvider : ActorProvider() {
+/** [ActorProvider] returned by [sample]. **/
+private val sampleActorProvider = object : ActorProvider() {
     override suspend fun remember(actor: Actor) {
     }
 
@@ -12,3 +12,7 @@ internal object SampleActorProvider : ActorProvider() {
         return Actor.Authenticated.sample
     }
 }
+
+/** [ActorProvider] that always provides a sample [authenticated][Actor.Authenticated] [Actor]. **/
+val ActorProvider.Companion.sample
+    get() = sampleActorProvider
--- core/sample/src/main/java/com/jeanbarrossilva/orca/core/sample/instance/Instance.extensions.kt
+++ core/sample/src/main/java/com/jeanbarrossilva/orca/core/sample/instance/Instance.extensions.kt
@@ -1,10 +1,12 @@
 package com.jeanbarrossilva.orca.core.sample.instance
 
 import com.jeanbarrossilva.orca.core.auth.AuthenticationLock
+import com.jeanbarrossilva.orca.core.auth.Authenticator
+import com.jeanbarrossilva.orca.core.auth.actor.ActorProvider
 import com.jeanbarrossilva.orca.core.instance.Instance
 import com.jeanbarrossilva.orca.core.instance.domain.Domain
 import com.jeanbarrossilva.orca.core.sample.auth.SampleAuthenticator
-import com.jeanbarrossilva.orca.core.sample.auth.actor.SampleActorProvider
+import com.jeanbarrossilva.orca.core.sample.auth.actor.sample
 import com.jeanbarrossilva.orca.core.sample.feed.SampleFeedProvider
 import com.jeanbarrossilva.orca.core.sample.feed.profile.SampleProfileProvider
 import com.jeanbarrossilva.orca.core.sample.feed.profile.search.SampleProfileSearcher
@@ -12,10 +14,10 @@ import com.jeanbarrossilva.orca.core.sample.feed.profile.toot.SampleTootProvider
 import com.jeanbarrossilva.orca.core.sample.instance.domain.sample
 
 /** [Instance] returned by [sample]. **/
-private val sampleInstance = object : Instance<SampleAuthenticator>() {
+private val sampleInstance = object : Instance<Authenticator>() {
     override val domain = Domain.sample
-    override val authenticator = SampleAuthenticator(SampleActorProvider)
-    override val authenticationLock = AuthenticationLock(authenticator, SampleActorProvider)
+    override val authenticator: Authenticator = SampleAuthenticator
+    override val authenticationLock = AuthenticationLock(authenticator, ActorProvider.sample)
     override val feedProvider = SampleFeedProvider
     override val profileProvider = SampleProfileProvider
     override val profileSearcher = SampleProfileSearcher
