95c33ff49de7d62e2e23142cdd1c0781a81ead19
Jean Silva <jeanbarrossilva@outlook.com>
2023-10-12 13:58:16 -0300

    refact: use spotless instead of ktfmt-gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,37 +1,38 @@
 rootProject.name = "Orca"
 
 pluginManagement.repositories {
-    google()
-    gradlePluginPortal()
-    mavenCentral()
+  google()
+  gradlePluginPortal()
+  mavenCentral()
 }
 
 includeBuild("build-src")
+
 include(
-    ":app",
-    ":core",
-    ":core:http",
-    ":core:sample",
-    ":core:sample-test",
-    ":core:shared-preferences",
-    ":core-test",
-    ":feature:composer",
-    ":feature:feed",
-    ":feature:profile-details",
-    ":feature:search",
-    ":feature:settings",
-    ":feature:settings:term-muting",
-    ":feature:toot-details",
-    ":platform:cache",
-    ":platform:launchable",
-    ":platform:theme",
-    ":platform:ui",
-    ":platform:ui-test",
-    ":std:image-loader",
-    ":std:image-loader:compose",
-    ":std:image-loader-test",
-    ":std:injector",
-    ":std:injector-processor",
-    ":std:injector-test",
-    ":std:styled-string"
+  ":app",
+  ":core",
+  ":core:http",
+  ":core:sample",
+  ":core:sample-test",
+  ":core:shared-preferences",
+  ":core-test",
+  ":feature:composer",
+  ":feature:feed",
+  ":feature:profile-details",
+  ":feature:search",
+  ":feature:settings",
+  ":feature:settings:term-muting",
+  ":feature:toot-details",
+  ":platform:cache",
+  ":platform:launchable",
+  ":platform:theme",
+  ":platform:ui",
+  ":platform:ui-test",
+  ":std:image-loader",
+  ":std:image-loader:compose",
+  ":std:image-loader-test",
+  ":std:injector",
+  ":std:injector-processor",
+  ":std:injector-test",
+  ":std:styled-string"
 )
