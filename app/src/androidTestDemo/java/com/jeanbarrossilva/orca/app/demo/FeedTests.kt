51b8c86135a2a5d376ac81368877a57058ff8531
Jean Silva <jeanbarrossilva@outlook.com>
2023-10-17 09:40:38 -0300

    refact: remove PlatformDialogDismissalTestRule

--- app/src/androidTestDemo/java/com/jeanbarrossilva/orca/app/demo/FeedTests.kt
+++ app/src/androidTestDemo/java/com/jeanbarrossilva/orca/app/demo/FeedTests.kt
@@ -8,7 +8,6 @@ import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.rule.IntentsRule
-import com.jeanbarrossilva.orca.app.demo.test.PlatformDialogDismissalTestRule
 import com.jeanbarrossilva.orca.app.demo.test.browsesTo
 import com.jeanbarrossilva.orca.app.demo.test.respondWithOK
 import com.jeanbarrossilva.orca.core.feed.profile.toot.content.highlight.Highlight
@@ -23,11 +22,8 @@ import org.junit.rules.RuleChain
 internal class FeedTests {
   private val intentsRule = IntentsRule()
   private val composeRule = createAndroidComposeRule<DemoOrcaActivity>()
-  private val platformDialogDismissalRule = PlatformDialogDismissalTestRule()
 
-  @get:Rule
-  val ruleChain: RuleChain? =
-    RuleChain.outerRule(intentsRule).around(composeRule).around(platformDialogDismissalRule)
+  @get:Rule val ruleChain: RuleChain? = RuleChain.outerRule(intentsRule).around(composeRule)
 
   @Test
   fun navigatesToTootHighlight() {
--- app/src/androidTestDemo/java/com/jeanbarrossilva/orca/app/demo/ProfileDetailsTests.kt
+++ app/src/androidTestDemo/java/com/jeanbarrossilva/orca/app/demo/ProfileDetailsTests.kt
@@ -10,7 +10,6 @@ import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.rule.IntentsRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.jeanbarrossilva.orca.app.R
-import com.jeanbarrossilva.orca.app.demo.test.PlatformDialogDismissalTestRule
 import com.jeanbarrossilva.orca.app.demo.test.browsesTo
 import com.jeanbarrossilva.orca.app.demo.test.performStartClick
 import com.jeanbarrossilva.orca.app.demo.test.respondWithOK
@@ -29,11 +28,8 @@ import org.junit.rules.RuleChain
 internal class ProfileDetailsTests {
   private val intentsRule = IntentsRule()
   private val composeRule = createAndroidComposeRule<DemoOrcaActivity>()
-  private val platformDialogDismissalRule = PlatformDialogDismissalTestRule()
 
-  @get:Rule
-  val ruleChain: RuleChain? =
-    RuleChain.outerRule(intentsRule).around(composeRule).around(platformDialogDismissalRule)
+  @get:Rule val ruleChain: RuleChain? = RuleChain.outerRule(intentsRule).around(composeRule)
 
   @Test
   fun navigatesToTootHighlight() {
