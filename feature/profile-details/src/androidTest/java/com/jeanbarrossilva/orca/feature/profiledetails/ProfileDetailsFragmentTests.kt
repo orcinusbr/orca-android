69c369d70f91f35eb975a28ef7f1b75be87ce8b0
Jean Silva <jeanbarrossilva@outlook.com>
2023-10-18 15:00:27 -0300

    refact: scroll to main action button on ProfileDetailsFragment tests

--- feature/profile-details/src/androidTest/java/com/jeanbarrossilva/orca/feature/profiledetails/ProfileDetailsFragmentTests.kt
+++ feature/profile-details/src/androidTest/java/com/jeanbarrossilva/orca/feature/profiledetails/ProfileDetailsFragmentTests.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.test.core.app.launchActivity
 import com.jeanbarrossilva.orca.core.feed.profile.type.followable.FollowableProfile
 import com.jeanbarrossilva.orca.core.sample.feed.profile.SampleProfileWriter
@@ -43,6 +44,7 @@ internal class ProfileDetailsFragmentTests {
       .use {
         composeRule
           .onNodeWithTag(ProfileDetails.Followable.MAIN_ACTION_BUTTON_TAG)
+          .performScrollTo()
           .performClick()
           .assertTextEquals(ProfileDetails.Followable.Status.UNFOLLOWED.label)
       }
