a3f03ef824031959baa0a91bc321886757b82d72
Jean Silva <jeanbarrossilva@outlook.com>
2023-10-18 09:03:13 -0300

    fix(build): missing Espresso Intents dependency

--- app/src/androidTestDemo/java/com/jeanbarrossilva/orca/app/demo/ProfileDetailsTests.kt
+++ app/src/androidTestDemo/java/com/jeanbarrossilva/orca/app/demo/ProfileDetailsTests.kt
@@ -2,17 +2,24 @@ package com.jeanbarrossilva.orca.app.demo
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.performClick
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.rule.IntentsRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.jeanbarrossilva.orca.app.R
+import com.jeanbarrossilva.orca.app.demo.test.browsesTo
 import com.jeanbarrossilva.orca.app.demo.test.performStartClick
+import com.jeanbarrossilva.orca.app.demo.test.respondWithOK
 import com.jeanbarrossilva.orca.core.feed.profile.toot.Toot
+import com.jeanbarrossilva.orca.core.feed.profile.toot.content.highlight.Highlight
 import com.jeanbarrossilva.orca.core.sample.feed.profile.toot.content.highlight.sample
 import com.jeanbarrossilva.orca.core.sample.feed.profile.toot.sample
 import com.jeanbarrossilva.orca.feature.tootdetails.TootDetailsFragment
 import com.jeanbarrossilva.orca.platform.ui.test.assertIsAtFragment
+import com.jeanbarrossilva.orca.platform.ui.test.component.timeline.toot.headline.onHeadlineCards
 import com.jeanbarrossilva.orca.platform.ui.test.component.timeline.toot.onTootPreviews
 import org.junit.Rule
 import org.junit.Test
