6cf0e62a8398d6aad89b385ac4f9571f817c3165
Jean Silva <jeanbarrossilva@outlook.com>
2023-10-18 16:33:26 -0300

    refact: request focus only when it's lost

--- app/src/androidTestDemo/java/com/jeanbarrossilva/orca/app/demo/test/TestDemoOrcaActivity.kt
+++ app/src/androidTestDemo/java/com/jeanbarrossilva/orca/app/demo/test/TestDemoOrcaActivity.kt
@@ -7,6 +7,12 @@ import com.jeanbarrossilva.orca.platform.ui.core.content
 internal class TestDemoOrcaActivity : DemoOrcaActivity() {
   override fun onWindowFocusChanged(hasFocus: Boolean) {
     super.onWindowFocusChanged(hasFocus)
-    content.children.first().requestFocus()
+    keepFocus(hasFocus)
+  }
+
+  private fun keepFocus(isFocused: Boolean) {
+    if (!isFocused) {
+      content.children.first().requestFocus()
+    }
   }
 }
