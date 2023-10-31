86064751b24d4557f135b099bcb4a04e7dd0c7d9
Jean Silva <jeanbarrossilva@outlook.com>
2023-10-31 18:50:23 -0300

    doc: complete Buildable's

--- std/buildable/src/main/java/com/jeanbarrossilva/orca/std/buildable/Buildable.kt
+++ std/buildable/src/main/java/com/jeanbarrossilva/orca/std/buildable/Buildable.kt
@@ -1,7 +1,31 @@
 package com.jeanbarrossilva.orca.std.buildable
 
 /**
- * Denotes that the annotated class should be able to be built through a builder from which each one
- * of its publicly declared members.
+ * Denotes that the annotated structure should be able to be built through a builder, from which
+ * each one of its constructor properties can have their values set and open methods can have their
+ * behavior defined.
+ *
+ * It allows for a more functional approach towards interfaces or abstract classes and primarily
+ * aims on improving overall readability.
+ *
+ * For example, a given class named `MyClass`, declared as
+ *
+ * ```kotlin
+ * @Buildable
+ * abstract class MyClass(a: Int) {
+ *   open fun f(b: Int) {
+ *     return a * b
+ *   }
+ * }
+ * ```
+ *
+ * can be instantiated by its generated factory method:
+ * ```kotlin
+ * MyClass(a = 64) {
+ *   f { b ->
+ *     a / b
+ *   }
+ * }
+ * ```
  */
 @Target(AnnotationTarget.CLASS) annotation class Buildable
