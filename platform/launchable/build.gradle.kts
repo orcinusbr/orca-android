7726f9977691cf26e1c29b0a01ac3318ae1774c4
Jean Silva <jeanbarrossilva@outlook.com>
2023-11-01 08:18:29 -0300

    refact: remove remaining :platform:launchable's directory

--- platform/launchable/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-  alias(libs.plugins.android.library)
-  alias(libs.plugins.kotlin.android)
-}
-
-android.defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
