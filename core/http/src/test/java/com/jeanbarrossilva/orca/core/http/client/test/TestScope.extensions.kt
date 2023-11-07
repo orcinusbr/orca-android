dd9eb82afe3525acca993eb2ce6bf8a2c633a2b5
Jean Silva <jeanbarrossilva@outlook.com>
2023-11-07 12:25:55 -0300

    refact: remove no-op call to HttpClient#authenticateAndGet

--- core/http/src/test/java/com/jeanbarrossilva/orca/core/http/client/test/TestScope.extensions.kt
+++ core/http/src/test/java/com/jeanbarrossilva/orca/core/http/client/test/TestScope.extensions.kt
@@ -5,7 +5,6 @@ import com.jeanbarrossilva.orca.core.auth.actor.Actor
 import com.jeanbarrossilva.orca.core.auth.actor.ActorProvider
 import com.jeanbarrossilva.orca.core.http.HttpModule
 import com.jeanbarrossilva.orca.core.http.client.CoreHttpClient
-import com.jeanbarrossilva.orca.core.http.client.authenticateAndGet
 import com.jeanbarrossilva.orca.core.http.client.test.instance.TestHttpInstance
 import com.jeanbarrossilva.orca.core.http.client.test.instance.TestHttpInstanceProvider
 import com.jeanbarrossilva.orca.core.module.CoreModule
@@ -19,7 +18,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.HttpRequest
 import io.ktor.http.HttpMethod
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 
@@ -117,7 +115,6 @@ private fun <T : Actor> runCoreHttpClientTest(
     ) {
       SampleTermMuter()
     }
-  runBlocking { instance.client.authenticateAndGet("") {} }
   Injector.register<CoreModule>(module)
   runTest { CoreHttpClientTestScope(delegate = this, instance.client, actor).body() }
   Injector.unregister<CoreModule>()
