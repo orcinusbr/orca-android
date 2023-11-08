86f02774d2bc495193a2b86960c44ad5f11adbc8
Jean Silva <jeanbarrossilva@outlook.com>
2023-11-08 08:47:57 -0300

    fix(bug): crash when launching app on debug variant

--- app/src/androidTest/java/com/jeanbarrossilva/orca/app/OrcaActivityTests.kt
+++ app/src/androidTest/java/com/jeanbarrossilva/orca/app/OrcaActivityTests.kt
@@ -1,10 +1,9 @@
 package com.jeanbarrossilva.orca.app
 
-import androidx.test.core.app.launchActivity
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.rule.IntentsRule
-import com.jeanbarrossilva.orca.app.test.TestOrcaActivity
+import com.jeanbarrossilva.orca.app.test.launchOrcaActivity
 import com.jeanbarrossilva.orca.core.http.auth.authorization.HttpAuthorizationActivity
 import org.junit.Rule
 import org.junit.Test
@@ -14,7 +13,7 @@ internal class OrcaActivityTests {
 
   @Test
   fun navigatesToAuthorization() {
-    launchActivity<TestOrcaActivity>().use {
+    launchOrcaActivity().use {
       intended(hasComponent(HttpAuthorizationActivity::class.qualifiedName))
     }
   }
