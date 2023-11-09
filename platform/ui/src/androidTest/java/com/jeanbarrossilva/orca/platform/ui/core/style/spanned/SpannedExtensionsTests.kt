1e761b2968d88867d87ef0656f7b7a573978ef88
Jean Silva <jeanbarrossilva@outlook.com>
2023-11-09 16:58:54 -0300

    add: Spanned#parts extension property

---
+++ platform/ui/src/androidTest/java/com/jeanbarrossilva/orca/platform/ui/core/style/spanned/SpannedExtensionsTests.kt
@@ -0,0 +1,24 @@
+package com.jeanbarrossilva.orca.platform.ui.core.style.spanned
+
+import android.graphics.Typeface
+import android.text.Html
+import android.text.style.StyleSpan
+import assertk.assertThat
+import assertk.assertions.containsExactly
+import org.junit.Test
+
+internal class SpannedExtensionsTests {
+  @Test
+  fun partitions() {
+    assertThat(
+        Html.fromHtml("<p><b><i>Hello</i></b>, <i>world</i>!</p>", Html.FROM_HTML_MODE_COMPACT)
+          .parts
+      )
+      .containsExactly(
+        Part(0..4).span(StyleSpan(Typeface.ITALIC)),
+        Part(0..4).span(StyleSpan(Typeface.BOLD)),
+        Part(5..6),
+        Part(7..11).span(StyleSpan(Typeface.ITALIC))
+      )
+  }
+}
--- platform/ui/src/main/java/com/jeanbarrossilva/orca/platform/ui/core/style/spanned/Spanned.extensions.kt
+++ platform/ui/src/main/java/com/jeanbarrossilva/orca/platform/ui/core/style/spanned/Spanned.extensions.kt
@@ -6,6 +6,17 @@ import com.jeanbarrossilva.orca.platform.ui.core.style.append
 import com.jeanbarrossilva.orca.std.styledstring.StyledString
 import com.jeanbarrossilva.orca.std.styledstring.buildStyledString
 
+/** [Part]s by which this [Spanned] is composed. */
+internal val Spanned.parts
+  get() =
+    mergeSpansIntoParts().fold(emptyList<Part>()) { accumulator, part ->
+      if (accumulator.isNotEmpty() && part.indices.first > accumulator.last().indices.last.inc()) {
+        accumulator + Part(accumulator.last().indices.last.inc()..part.indices.first.dec()) + part
+      } else {
+        accumulator + part
+      }
+    }
+
 /** Converts this [Spanned] into a [StyledString]. */
 fun Spanned.toStyledString(): StyledString {
   return buildStyledString {
@@ -13,4 +24,15 @@ fun Spanned.toStyledString(): StyledString {
       getSpans<Any>(start = index, end = index).onEach { append(it, char) }.ifEmpty { +char }
     }
   }
+}
+
+/**
+ * Merges all spans that have been applied to this [Spanned] into ordered spanned [Part]s, from
+ * which the spans and also their respective indices can be obtained.
+ *
+ * @see Part.Spanned
+ * @see Part.Spanned.getIndices
+ */
+private fun Spanned.mergeSpansIntoParts(): List<Part.Spanned> {
+  return getSpans<Any>().map { Part(getSpanStart(it)..getSpanEnd(it).dec()).span(it) }
 }
