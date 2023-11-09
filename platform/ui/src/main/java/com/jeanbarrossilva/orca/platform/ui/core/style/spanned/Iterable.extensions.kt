6207baca1cb19db99b9d6410040e4be3e7058dfd
Jean Silva <jeanbarrossilva@outlook.com>
2023-11-09 15:36:45 -0300

    refact: use Int instead of AtomicReference<Int> in consecutively

--- platform/ui/src/main/java/com/jeanbarrossilva/orca/platform/ui/core/style/spanned/Iterable.extensions.kt
+++ platform/ui/src/main/java/com/jeanbarrossilva/orca/platform/ui/core/style/spanned/Iterable.extensions.kt
@@ -1,7 +1,5 @@
 package com.jeanbarrossilva.orca.platform.ui.core.style.spanned
 
-import java.util.concurrent.atomic.AtomicReference
-
 /**
  * Groups [Int]s that are consecutive into [IntRange]s, sorting them by their first value
  * ascendingly.
@@ -11,27 +9,27 @@ internal fun Iterable<Int>.consecutively(): List<IntRange> {
   if (count == 0) {
     return emptyList()
   }
-  val (start, end) = AtomicReference<Int>() to AtomicReference<Int>()
+  var (start, end) = null as Int? to null as Int?
   val grouped = arrayOfNulls<IntRange>(size = count)
   var groupIndex = 0
-  val group = { grouped[groupIndex++] = start.get()!!..end.get()!! }
+  val group = { grouped[groupIndex++] = start!!..end!! }
   val isLastIntIndex = { intIndex: Int -> intIndex == count - 1 }
   forEachIndexed { intIndex, int ->
-    if (start.get() == null) {
-      start.set(int)
-    } else if (end.get() == null || int == end.get()!!.inc()) {
-      end.set(int)
+    if (start == null) {
+      start = int
+    } else if (end == null || int == end!!.inc()) {
+      end = int
       if (isLastIntIndex(intIndex)) {
         group()
       }
     } else {
       group()
-      start.set(int)
+      start = int
       if (isLastIntIndex(intIndex)) {
-        end.set(int)
+        end = int
         group()
       } else {
-        end.set(null)
+        end = null
       }
     }
   }
