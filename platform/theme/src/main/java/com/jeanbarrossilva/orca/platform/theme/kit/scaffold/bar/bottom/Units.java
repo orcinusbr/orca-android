e73fcefbc0651c590217ffd21155793b31ab153e
Jean Silva <jeanbarrossilva@outlook.com>
2023-11-10 18:26:36 -0300

    refact: Google-Java format Java files

--- platform/theme/src/main/java/com/jeanbarrossilva/orca/platform/theme/kit/scaffold/bar/bottom/Units.java
+++ platform/theme/src/main/java/com/jeanbarrossilva/orca/platform/theme/kit/scaffold/bar/bottom/Units.java
@@ -4,22 +4,19 @@ import android.content.Context;
 import android.util.TypedValue;
 import androidx.annotation.NonNull;
 
-/** Utilities for performing operations with units. **/
+/** Utilities for performing operations with units. */
 class Units {
-    private Units() {
-    }
+  private Units() {}
 
-    /**
-     * Converts the DPs into pixels.
-     *
-     * @param context {@link Context} through which the conversion will take place.
-     * @param dp Amount in DPs to be converted into pixels.
-     */
-    static int dp(@NonNull Context context, int dp) {
-        return (int) TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            dp,
-            context.getResources().getDisplayMetrics()
-        );
-    }
+  /**
+   * Converts the DPs into pixels.
+   *
+   * @param context {@link Context} through which the conversion will take place.
+   * @param dp Amount in DPs to be converted into pixels.
+   */
+  static int dp(@NonNull Context context, int dp) {
+    return (int)
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+  }
 }
