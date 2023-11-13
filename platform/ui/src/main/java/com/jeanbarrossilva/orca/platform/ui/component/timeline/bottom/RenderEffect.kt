1112d67199346bc30b0559f999560e4452a4de5c
Jean Silva <jeanbarrossilva@outlook.com>
2023-11-13 09:03:49 -0300

    refact: remove unused imports

--- platform/ui/src/main/java/com/jeanbarrossilva/orca/platform/ui/component/timeline/bottom/RenderEffect.kt
+++ platform/ui/src/main/java/com/jeanbarrossilva/orca/platform/ui/component/timeline/bottom/RenderEffect.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import com.jeanbarrossilva.orca.platform.ui.component.timeline.Timeline
