f14e719a93589463d3c9ef4ef8235ad7104c6030
Jean Silva <jeanbarrossilva@outlook.com>
2023-11-16 13:38:22 -0300

    refact(perf): unblock main thread on image loading

--- std/image-loader/local/src/main/java/com/jeanbarrossilva/orca/std/imageloader/local/Bitmap.extensions.kt
+++ std/image-loader/local/src/main/java/com/jeanbarrossilva/orca/std/imageloader/local/Bitmap.extensions.kt
@@ -3,18 +3,25 @@ package com.jeanbarrossilva.orca.std.imageloader.local
 import android.graphics.Bitmap
 import com.jeanbarrossilva.orca.std.imageloader.Image
 import com.jeanbarrossilva.orca.std.imageloader.buildImage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 
 /** Converts this [Bitmap] into an [Image]. */
-fun Bitmap.toImage(): Image {
-  return buildImage(width, height) {
-    val nonHardwareBitmap =
-      if (config == Bitmap.Config.HARDWARE) {
-        copy(Bitmap.Config.ARGB_8888, false)
-      } else {
-        this@toImage
+suspend fun Bitmap.toImage(): Image {
+  return withContext(Dispatchers.IO) {
+    coroutineScope {
+      buildImage(width, height) {
+        val nonHardwareBitmap =
+          if (config == Bitmap.Config.HARDWARE) {
+            copy(Bitmap.Config.ARGB_8888, false)
+          } else {
+            this@toImage
+          }
+        IntArray(size = width * height)
+          .also { nonHardwareBitmap.getPixels(it, 0, width, 0, 0, width, height) }
+          .forEach(::pixel)
       }
-    IntArray(size = width * height)
-      .also { nonHardwareBitmap.getPixels(it, 0, width, 0, 0, width, height) }
-      .forEach(::pixel)
+    }
   }
 }
