708a4535bfd3b0601c631dd38ac5d3b28e6b08f8
Jean Silva <jeanbarrossilva@outlook.com>
2023-12-01 10:31:33 -0300

    config: add GNU GPL-3.0 License

--- platform/ui/src/main/java/com/jeanbarrossilva/orca/platform/ui/component/timeline/post/stat/PostPreview.Stat.Favorite.kt
+++ platform/ui/src/main/java/com/jeanbarrossilva/orca/platform/ui/component/timeline/post/stat/PostPreview.Stat.Favorite.kt
@@ -1,3 +1,17 @@
+/*
+ * Copyright © 2023 Orca
+ *
+ * Licensed under the GNU General Public License, Version 3 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *                        https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.jeanbarrossilva.orca.platform.ui.component.timeline.post.stat
 
 import androidx.compose.animation.animateColorAsState
