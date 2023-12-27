f537adc661cec3d57efaf8aeb57cae222a98f249
Jean Silva <jeanbarrossilva@outlook.com>
2023-12-27 15:01:30 -0300

    test: edge-to-edge gallery

--- feature/gallery/src/androidTest/java/com/jeanbarrossilva/orca/feature/gallery/GalleryActivityTests.kt
+++ feature/gallery/src/androidTest/java/com/jeanbarrossilva/orca/feature/gallery/GalleryActivityTests.kt
@@ -15,8 +15,16 @@
 
 package com.jeanbarrossilva.orca.feature.gallery
 
+import android.util.TypedValue
+import android.view.WindowInsets
 import androidx.compose.ui.test.assertContentDescriptionEquals
+import androidx.compose.ui.test.assertHeightIsEqualTo
+import androidx.compose.ui.test.assertWidthIsEqualTo
 import androidx.compose.ui.test.junit4.createEmptyComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.test.platform.app.InstrumentationRegistry
 import com.jeanbarrossilva.orca.feature.gallery.test.activity.TestGalleryModule
 import com.jeanbarrossilva.orca.feature.gallery.test.launchGalleryActivity
@@ -29,14 +37,44 @@ import org.junit.Rule
 import org.junit.Test
 
 internal class GalleryActivityTests {
+  private val context
+    get() = InstrumentationRegistry.getInstrumentation().context
+
   @get:Rule
   val injectorRule = InjectorTestRule { register(TestGalleryModule.boundTo<GalleryModule, _>()) }
 
   @get:Rule val composeRule = createEmptyComposeRule()
 
   @Test
+  fun isEdgeToEdge() {
+    val density = Density(context)
+    val resources = context.resources
+    var systemBarsHeight = Dp.Unspecified
+    val actionBarHeight =
+      TypedValue()
+        .also { context.theme.resolveAttribute(android.R.attr.actionBarSize, it, true) }
+        .let { resources.getDimensionPixelSize(it.resourceId) }
+        .let { with(density) { it.toDp() } }
+    val configuration = resources.configuration
+    launchGalleryActivity().use { scenario ->
+      scenario.onActivity { activity ->
+        activity.windowManager
+          ?.currentWindowMetrics
+          ?.windowInsets
+          ?.getInsets(WindowInsets.Type.systemBars())
+          ?.run { systemBarsHeight = with(density) { (top + bottom).toDp() } }
+      }
+      composeRule
+        .onRoot()
+        .assertWidthIsEqualTo(configuration.screenWidthDp.dp)
+        .assertHeightIsEqualTo(
+          configuration.screenHeightDp.dp + (systemBarsHeight - actionBarHeight)
+        )
+    }
+  }
+
+  @Test
   fun describesEachPage() {
-    val context = InstrumentationRegistry.getInstrumentation().context
     launchGalleryActivity().use {
       with(composeRule) {
         onPager().performScrollToEachPage {
