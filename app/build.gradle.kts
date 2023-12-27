6e0a027b0bfba3bed4c96b5d2bea543147bb4d54
Jean Silva <jeanbarrossilva@outlook.com>
2023-12-27 09:04:59 -0300

    version: change to 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
     minSdk = libs.versions.android.sdk.min.get().toInt()
     targetSdk = libs.versions.android.sdk.target.get().toInt()
     versionCode = 1
-    versionName = "1.0.0"
+    versionName = "0.1.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
