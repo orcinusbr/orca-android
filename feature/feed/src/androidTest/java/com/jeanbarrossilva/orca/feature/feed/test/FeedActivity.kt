6b8eb9b862a31181ef8ec10c804131a797e196f7
Jean Silva <jeanbarrossilva@outlook.com>
2023-12-28 12:16:15 -0300

    refact: use :platform-testing API at :feature:feed

--- feature/feed/src/androidTest/java/com/jeanbarrossilva/orca/feature/feed/test/FeedActivity.kt
+++ feature/feed/src/androidTest/java/com/jeanbarrossilva/orca/feature/feed/test/FeedActivity.kt
@@ -17,8 +17,8 @@ package com.jeanbarrossilva.orca.feature.feed.test
 
 import android.content.Intent
 import androidx.navigation.NavGraphBuilder
-import androidx.test.platform.app.InstrumentationRegistry
 import com.jeanbarrossilva.orca.feature.feed.FeedFragment
+import com.jeanbarrossilva.orca.platform.testing.context
 import com.jeanbarrossilva.orca.platform.ui.core.Intent
 import com.jeanbarrossilva.orca.platform.ui.test.core.SingleFragmentActivity
 
@@ -31,7 +31,6 @@ internal class FeedActivity : SingleFragmentActivity() {
 
   companion object {
     fun getIntent(userID: String): Intent {
-      val context = InstrumentationRegistry.getInstrumentation().context
       return Intent<FeedActivity>(context, FeedFragment.USER_ID_KEY to userID)
     }
   }
