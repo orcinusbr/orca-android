68f51c976155757b23ffa11a72f3a53444c05cfa
Jean Silva <jeanbarrossilva@outlook.com>
2023-12-28 11:45:06 -0300

    add: :platform:testing

---
+++ platform/testing/src/androidTest/java/com/jeanbarrossilva/orca/platform/testing/IntExtensionsTests.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2023 Orca
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see https://www.gnu.org/licenses.
+ */
+
+package com.jeanbarrossilva.orca.platform.testing
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.jeanbarrossilva.orca.platform.testing.test.R
+import org.junit.Test
+
+internal class IntExtensionsTests {
+  @Test
+  fun getsStringFromResourceID() {
+    assertThat(R.string.string.asString()).isEqualTo("5️⃣👍🏽🏞️👩🏻‍💻⏩🎇")
+  }
+}
