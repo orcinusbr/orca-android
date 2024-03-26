3012e7b25554b108f1e6eeac2cb3b73a1f9216af
Jean Silva <jeanbarrossilva@outlook.com>
2024-03-26 12:01:33 -0300

    fix(bug): Stack Y offsets and scales calculated incorrectly for more than 2 items

--- feature/registration/src/main/java/com/jeanbarrossilva/orca/feature/registration/ui/stack/Stack.kt
+++ feature/registration/src/main/java/com/jeanbarrossilva/orca/feature/registration/ui/stack/Stack.kt
@@ -16,11 +16,13 @@
 package com.jeanbarrossilva.orca.feature.registration.ui.stack
 
 import androidx.annotation.FloatRange
+import androidx.annotation.IntRange
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.util.fastForEachIndexed
 import com.jeanbarrossilva.orca.feature.registration.ui.stack.state.StackState
 import com.jeanbarrossilva.orca.feature.registration.ui.stack.state.rememberStackState
 import com.jeanbarrossilva.orca.feature.registration.ui.status.Status
@@ -28,28 +30,50 @@ import com.jeanbarrossilva.orca.feature.registration.ui.status.StatusCard
 import com.jeanbarrossilva.orca.feature.registration.ui.status.rememberStatusCardState
 import com.jeanbarrossilva.orca.platform.autos.theme.AutosTheme
 import com.jeanbarrossilva.orca.platform.autos.theme.MultiThemePreview
-import kotlin.math.abs
+import kotlin.math.pow
 import kotlin.math.roundToInt
 
 /**
  * Maximum amount of items that are visible within a [Stack], including both foreground and
  * background ones.
  */
-private const val MAX_VISIBLE_ITEM_COUNT = 3
+private const val MaxVisibleItemCount = 3
 
 /** Maximum amount of background items that are visible. */
-private const val MAX_VISIBLE_BACKGROUND_ITEM_COUNT = MAX_VISIBLE_ITEM_COUNT - 1
-
-/** Fraction by which the first background item is scaled. */
-@FloatRange(from = 0.0, to = 1.0) private const val FIRST_BACKGROUND_ITEM_SCALE = .95f
+private const val MaxVisibleBackgroundItemCount = MaxVisibleItemCount - 1
 
 /**
- * Fraction in the Y axis of the first background item that is visible. Also used as a basis for
- * calculating the fraction of all subsequent background items.
- *
- * @see calculateYOffsetForBackgroundItem
+ * Amount to be subtracted from the quantity of total items that have been added onto a [Stack] for
+ * the index of the first visible background one to be determined.
  */
-@FloatRange(from = 0.0, to = 1.0) private const val FIRST_BACKGROUND_ITEM_VISIBILITY_FRACTION = .1f
+private const val FurthermostVisibleBackgroundItemIndexSubtrahend =
+  MaxVisibleItemCount - MaxVisibleBackgroundItemCount
+
+/**
+ * Fraction by which the first background item is scaled, based on which following ones will also be
+ * scaled.
+ */
+@FloatRange(from = 0.0, to = 1.0) private const val InitialBackgroundItemScale = .95f
+
+/**
+ * Fraction in the Y axis by which the furthermost background item is offset. Also used as a basis
+ * for calculating the Y offset of all subsequent background items.
+ *
+ * @see calculateUnscaledYOffsetForItem
+ * @see calculateScaledYOffsetForBackgroundItem
+ */
+@FloatRange(from = 0.0, to = 1.0) private const val InitialBackgroundItemYOffsetFraction = .1f
+
+/**
+ * Denotes that an [Int] is the index of a background item.
+ *
+ * For an explanation on what foreground and background items are, refer both to [Stack]'s and
+ * [requireBackgroundItemIndex]'s documentations.
+ */
+@IntRange(from = 0, to = MaxVisibleBackgroundItemCount.toLong())
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.VALUE_PARAMETER)
+private annotation class BackgroundItemIndex
 
 /**
  * Stacks each of its added items on top of each other, with the one that has been added the most
@@ -57,7 +81,7 @@ private const val MAX_VISIBLE_BACKGROUND_ITEM_COUNT = MAX_VISIBLE_ITEM_COUNT - 1
  *
  * Realistically, only a certain amount of items will be rendered: the one added lastly (referred to
  * as the "foreground" one) and some of those previous to it (the "background" ones). The exact
- * quantity is determined by [MAX_VISIBLE_ITEM_COUNT].
+ * quantity is determined by [MaxVisibleItemCount].
  *
  * @param modifier [Modifier] that is applied to the underlying [Layout].
  * @param state [StackState] to which items can be added. Also the one provided to the [content].
@@ -76,78 +100,147 @@ internal fun Stack(
   }
 
   Layout({ state.contents.forEach { it() } }, modifier) { measurables, constraints ->
-    val items = measurables.takeLast(MAX_VISIBLE_ITEM_COUNT).map { it.measure(constraints) }
+    val items = measurables.takeLast(MaxVisibleItemCount).map { it.measure(constraints) }
     val foreground = items.lastOrNull() ?: return@Layout layout(0, 0) {}
-    val background = items.take(MAX_VISIBLE_BACKGROUND_ITEM_COUNT)
-    val backgroundYOffsets =
-      background
-        .mapIndexed { index, item -> IndexedValue(index.inc(), item) }
-        .associate { (index, item) ->
-          index to calculateYOffsetForBackgroundItem(index, item.height)
-        }
-    val backgroundHeight =
-      backgroundYOffsets.values.withIndex().sumOf { (index, yOffset) ->
-        scaleYOffsetForBackgroundItem(index, yOffset)
+    val foregroundIndex = items.lastIndex
+    val foregroundWidth = foreground.width
+    val foregroundHeight = foreground.height
+    val foregroundYOffset = calculateUnscaledYOffsetForItem(foregroundIndex, foregroundHeight)
+    val backgroundCount = items.size - FurthermostVisibleBackgroundItemIndexSubtrahend
+    val background = items.take(backgroundCount)
+    val backgroundScales =
+      List(backgroundCount) { calculateScaleForBackgroundItem(backgroundCount, index = it) }
+    val backgroundScaledYOffsets =
+      background.mapIndexed { index, item ->
+        calculateScaledYOffsetForBackgroundItem(
+          backgroundCount,
+          index,
+          item.height,
+          backgroundScales[index]
+        )
       }
-    layout(foreground.width, foreground.height + backgroundHeight) {
-      with(items) {
-        forEachIndexed { index, item ->
-          item.placeWithLayer(
-            x = calculateCenteringXForItem(foreground.width, item.width),
-            y = take(index).fold(0) { itemY, _ -> itemY - (backgroundYOffsets[index] ?: 0) },
-            zIndex = index.toFloat() - items.lastIndex.toFloat()
-          ) {
-            val isBackgroundItem = index < items.lastIndex
-            if (isBackgroundItem) {
-              scaleX = calculateScaleForBackgroundItem(index)
-              scaleY = scaleX
-            }
-          }
+    val backgroundHeight = backgroundScaledYOffsets.sum()
+    layout(foregroundWidth, backgroundHeight + foregroundHeight + foregroundYOffset) {
+      background.fastForEachIndexed { index, backgroundItem ->
+        backgroundItem.placeWithLayer(
+          x = calculateCenteringXForBackgroundItem(foregroundWidth, backgroundItem.width),
+          y = backgroundScaledYOffsets[index] - foregroundYOffset
+        ) {
+          scaleX = backgroundScales[index]
+          scaleY = scaleX
         }
       }
+      foreground.place(x = 0, y = backgroundHeight + foregroundYOffset)
     }
   }
 }
 
 /**
- * Scales the [verticalOffset] of a background item according to the index at which it is.
+ * Calculates the scaled Y offset of a background item according to the index at which it is.
  *
- * @param index Index at which the background item whose [verticalOffset] will be scaled is.
- * @param verticalOffset Amount to be scaled based on the [FIRST_BACKGROUND_ITEM_SCALE], by which
- *   the background item has been offset in the Y axis.
+ * @param count Amount of background items being shown.
+ * @param index Index at which the background item whose unscaled Y offset will be scaled is.
+ * @param height Height of the background item, as seen by the parent layout.
+ * @param scale Amount by which the unscaled Y offset will be scaled.
+ * @throws IllegalArgumentException If the [index] isn't that of a background item, as per
+ *   [requireBackgroundItemIndex]'s documentation.
+ * @see calculateUnscaledYOffsetForItem
  */
-private fun scaleYOffsetForBackgroundItem(index: Int, verticalOffset: Int): Int {
-  val absVerticalOffset = abs(verticalOffset)
-  return absVerticalOffset - (FIRST_BACKGROUND_ITEM_SCALE * absVerticalOffset).roundToInt() * index
+@Throws(IllegalArgumentException::class)
+private fun calculateScaledYOffsetForBackgroundItem(
+  count: Int,
+  @BackgroundItemIndex index: Int,
+  height: Int,
+  @FloatRange(from = 0.0, to = 1.0) scale: Float = calculateScaleForBackgroundItem(count, index)
+): Int {
+  requireBackgroundItemIndex(index)
+  val unscaledYOffset = calculateUnscaledYOffsetForItem(index, height)
+  return (unscaledYOffset * scale).roundToInt()
 }
 
 /**
- * Calculates the offset in the Y axis of a background item.
+ * Calculates the unscaled offset in the Y axis of an item (either a foreground or a background
+ * one).
  *
- * @param index Index at which the background item to be placed is.
- * @param height Height of the background item, as seen by the parent layout.
+ * @param index Index at which the item to be placed is.
+ * @param height Height of the item, as seen by the parent layout.
+ * @see InitialBackgroundItemYOffsetFraction
  */
-private fun calculateYOffsetForBackgroundItem(index: Int, height: Int): Int {
-  return -(FIRST_BACKGROUND_ITEM_VISIBILITY_FRACTION * index.inc() * height).roundToInt()
+private fun calculateUnscaledYOffsetForItem(index: Int, height: Int): Int {
+  return if (index == 0) 0 else (InitialBackgroundItemYOffsetFraction / index * height).roundToInt()
 }
 
 /**
  * Calculates the scale for a background item.
  *
+ * @param count Amount of background items being shown.
  * @param index Index at which the background item to be scaled is.
+ * @throws IllegalArgumentException If the [index] isn't that of a background item, as per
+ *   [requireBackgroundItemIndex]'s documentation.
  */
-private fun calculateScaleForBackgroundItem(index: Int): Float {
-  return FIRST_BACKGROUND_ITEM_SCALE / index.inc()
+@Throws(IllegalArgumentException::class)
+@FloatRange(from = 0.0, to = 1.0)
+private fun calculateScaleForBackgroundItem(count: Int, @BackgroundItemIndex index: Int): Float {
+  val reversedIndex = calculateReversedIndexForBackgroundItem(count, index)
+  return InitialBackgroundItemScale.pow(reversedIndex)
 }
 
 /**
- * Calculates a coordinate in the X axis for an item to be horizontally centered within its parent.
+ * Calculates the reversed index of a background item, as if the stacking order was the inverse of
+ * what it is.
+ *
+ * In a [Stack] with 3 items, for example, one background item and two background ones, the item
+ * that was added first (which would visually be the furthermost item) would normally be indexed as
+ * 0; this method, on the other hand, returns 2 for it.
+ *
+ * @param count Amount of background items being shown.
+ * @param index Index to be reversed, at which the background item is.
+ * @throws IllegalArgumentException If the [index] isn't that of a background item, as per
+ *   [requireBackgroundItemIndex]'s documentation.
+ */
+@Throws(IllegalArgumentException::class)
+private fun calculateReversedIndexForBackgroundItem(
+  count: Int,
+  @BackgroundItemIndex index: Int
+): Int {
+  requireBackgroundItemIndex(index)
+  return count - index
+}
+
+/**
+ * Requires the given [index] to be that of a [Stack]'s background item, meaning that it should be
+ * an [Int] within the `0..<`[MaxVisibleBackgroundItemCount] range. It starts at 0 because the item
+ * that has been added lastly, the foreground one, is the one that is the most visible and has the
+ * greatest index. That essentially means that each added item will be on top of the previously
+ * added one and will have an increased index compared to the one preceding it.
+ *
+ * The furthermost background item, located at index `n -
+ * `[FurthermostVisibleBackgroundItemIndexSubtrahend] within the entire item [List] (`n` being the
+ * total amount of both foreground and background items), has an index of 0 among the items that are
+ * actually visible (which does not always encompass every added one).
+ *
+ * @param index Index at which the background item is.
+ * @throws IllegalArgumentException If the index isn't that of a background item (that is, isn't
+ *   within the `0..<`[MaxVisibleBackgroundItemCount]` range).
+ * @see Int.inc
+ */
+@Throws(IllegalArgumentException::class)
+internal fun requireBackgroundItemIndex(@BackgroundItemIndex index: Int) {
+  require(index >= 0) { "Index of a background item should be >= 0." }
+  require(index < MaxVisibleBackgroundItemCount) {
+    "Index of a background item cannot be greater than $MaxVisibleBackgroundItemCount."
+  }
+}
+
+/**
+ * Calculates a coordinate in the X axis for a background item to be horizontally centered within
+ * its parent.
  *
  * @param parentWidth Width of the parent layout in which the child will be placed.
- * @param childWidth Width of the child layout to be placed.
+ * @param backgroundItemWidth Width of the child layout to be placed.
  */
-private fun calculateCenteringXForItem(parentWidth: Int, childWidth: Int): Int {
-  return (parentWidth - childWidth) / 2
+private fun calculateCenteringXForBackgroundItem(parentWidth: Int, backgroundItemWidth: Int): Int {
+  return (parentWidth - backgroundItemWidth) / 2
 }
 
 /** Preview of a [Stack]. */
@@ -157,7 +250,8 @@ private fun StackPreview() {
   AutosTheme {
     Stack {
       item { StatusCard(rememberStatusCardState(Status.Failed)) { Text("Card #1") } }
-      item { StatusCard(rememberStatusCardState(Status.Succeeded)) { Text("Card #2") } }
+      item { StatusCard(rememberStatusCardState(Status.Failed)) { Text("Card #2") } }
+      item { StatusCard(rememberStatusCardState(Status.Succeeded)) { Text("Card #3") } }
     }
   }
 }
