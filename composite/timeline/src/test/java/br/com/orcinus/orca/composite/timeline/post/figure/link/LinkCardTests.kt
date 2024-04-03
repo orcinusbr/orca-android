91f8aeae776c7aa375ebf267e10e59c0764079cb
Jean Silva <jeanbarrossilva@outlook.com>
2024-04-03 10:40:10 -0300

    refact: use Robolectric at :composite:timeline

---
+++ composite/timeline/src/test/java/br/com/orcinus/orca/composite/timeline/post/figure/link/LinkCardTests.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2023–2024 Orcinus
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see https://www.gnu.org/licenses.
+ */
+
+package br.com.orcinus.orca.composite.timeline.post.figure.link
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.performClick
+import br.com.orcinus.orca.composite.timeline.test.post.figure.link.onLinkCard
+import br.com.orcinus.orca.platform.autos.theme.AutosTheme
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+internal class LinkCardTests {
+  @get:Rule val composeRule = createComposeRule()
+
+  @Test
+  fun runsCallbackWhenClicked() {
+    var hasCallbackBeenRun = false
+    composeRule.setContent { AutosTheme { LinkCard(onClick = { hasCallbackBeenRun = true }) } }
+    composeRule.onLinkCard().performClick()
+    assertTrue(hasCallbackBeenRun)
+  }
+}
---
+++ composite/timeline/src/test/java/br/com/orcinus/orca/composite/timeline/post/time/Time4JRelativeTimeProviderTests.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2024 Orcinus
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see https://www.gnu.org/licenses.
+ */
+
+package br.com.orcinus.orca.composite.timeline.post.time
+
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import io.mockk.mockkStatic
+import io.mockk.verify
+import java.time.ZonedDateTime
+import net.time4j.android.ApplicationStarter
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+internal class Time4JRelativeTimeProviderTests {
+  @Test
+  fun startsTime4JOnce() {
+    val context: Context = InstrumentationRegistry.getInstrumentation().context
+    val provider = Time4JRelativeTimeProvider(context)
+    val zonedDateTime = ZonedDateTime.now()
+    mockkStatic(ApplicationStarter::class) {
+      repeat(2) { provider.provide(zonedDateTime) }
+      verify(exactly = 1) { ApplicationStarter.initialize(context, any<Boolean>()) }
+    }
+  }
+}
