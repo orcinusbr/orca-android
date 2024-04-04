bcf4816823763add01d0ccda4b6d9ee4139e1d72
Jean Silva <jeanbarrossilva@outlook.com>
2024-04-03 21:32:17 -0300

    fix(build): missing :feature:feed androidx.fragment:fragment-ktx implementation

--- feature/feed/build.gradle.kts
+++ feature/feed/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
   implementation(project(":platform:intents"))
   implementation(project(":platform:navigation"))
   implementation(project(":std:injector"))
+  implementation(libs.android.fragment.ktx)
   implementation(libs.loadable.list)
 
   testImplementation(project(":composite:timeline-test"))
