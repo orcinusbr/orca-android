ce7ff74d3ecee240cc7fe01d08819ae0066f3592
Jean Silva <jeanbarrossilva@outlook.com>
2024-05-05 21:00:30 -0300

    Fix sequential navigation test failure

--- platform/navigation/src/test/java/br/com/orcinus/orca/platform/navigation/NavigatorTests.kt
+++ platform/navigation/src/test/java/br/com/orcinus/orca/platform/navigation/NavigatorTests.kt
@@ -39,7 +39,7 @@ internal class NavigatorTests {
     }
   }
 
-  class SecondDestinationFragment : DestinationFragment(::generateID) {
+  class SecondDestinationFragment : DestinationFragment({ id }) {
     companion object {
       val id = generateID()
     }
