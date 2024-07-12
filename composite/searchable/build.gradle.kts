ac6302dba6c0387316e4945beed876f94d941466
Jean Silva <jeanbarrossilva@outlook.com>
2024-07-12 16:50:33 -0300

    Test `Dismissible`

--- composite/searchable/build.gradle.kts
+++ composite/searchable/build.gradle.kts
@@ -4,10 +4,19 @@ plugins {
   alias(libs.plugins.kotlin.compose)
 }
 
-android.buildFeatures.compose = true
+android {
+  buildFeatures.compose = true
+  testOptions.unitTests.isIncludeAndroidResources = true
+}
 
 dependencies {
   implementation(project(":platform:autos"))
   implementation(project(":platform:focus"))
   implementation(libs.android.constraintLayout.compose)
+
+  testImplementation(libs.android.compose.ui.test.junit)
+  testImplementation(libs.android.compose.ui.test.manifest)
+  testImplementation(libs.assertk)
+  testImplementation(libs.kotlin.test)
+  testImplementation(libs.robolectric)
 }
