f21697e5d73b9cee2e502ffd2b914ba3bb706d9a
Jean Silva <jeanbarrossilva@outlook.com>
2024-08-01 05:29:50 -0300

    Rename `SemanticsNodeInteractionsProviderExtensionsTests.findsIndicator()` to "findRefreshIndicator"

--- platform/autos-test/src/test/java/br/com/orcinus/orca/platform/autos/test/overlays/refresh/SemanticsNodeInteractionsProviderExtensionsTests.kt
+++ platform/autos-test/src/test/java/br/com/orcinus/orca/platform/autos/test/overlays/refresh/SemanticsNodeInteractionsProviderExtensionsTests.kt
@@ -29,7 +29,7 @@ internal class SemanticsNodeInteractionsProviderExtensionsTests {
   @get:Rule val composeRule = createComposeRule()
 
   @Test
-  fun findsIndicator() {
+  fun findsRefreshIndicator() {
     composeRule
       .apply { setContent { Refreshable(Refresh.Indefinite) {} } }
       .onRefreshIndicator()
