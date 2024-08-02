ae06c0dd7239a8037f669b8088e7c868755d407d
Jean Silva <jeanbarrossilva@outlook.com>
2024-08-02 20:08:03 -0300

    Test parallel unlocks

--- core/src/test/java/br/com/orcinus/orca/core/auth/AuthenticationLockTests.kt
+++ core/src/test/java/br/com/orcinus/orca/core/auth/AuthenticationLockTests.kt
@@ -2,15 +2,30 @@ package br.com.orcinus.orca.core.auth
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isSameAs
 import assertk.assertions.isTrue
+import br.com.orcinus.orca.core.auth.actor.Actor
+import br.com.orcinus.orca.core.auth.actor.FixedActorProvider
+import br.com.orcinus.orca.core.sample.test.auth.actor.sample
 import br.com.orcinus.orca.core.test.TestActorProvider
 import br.com.orcinus.orca.core.test.TestAuthenticationLock
 import br.com.orcinus.orca.core.test.TestAuthenticator
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 
 internal class AuthenticationLockTests {
@@ -47,6 +62,39 @@ internal class AuthenticationLockTests {
           testScheduler.currentTime.milliseconds.inWholeSeconds
         )
         .isEqualTo(8_224)
+    }
+  }
+
+  @Test
+  fun unlocksWhileAnotherUnlockIsOngoing() {
+    runTest {
+      val actorProvider = FixedActorProvider(Actor.Authenticated.sample)
+      val authenticator = TestAuthenticator(actorProvider = actorProvider)
+      val lock = TestAuthenticationLock(actorProvider, authenticator)
+      val scheduleCount = 2
+      val actors =
+        flow {
+            var initialSchedulingContinuation: Continuation<Unit>? = null
+            val initialSchedulingSuspensionJob =
+              launch(Dispatchers.Unconfined, CoroutineStart.LAZY) {
+                suspendCoroutine { initialSchedulingContinuation = it }
+              }
+            repeat(scheduleCount) { _ ->
+              emit(
+                async {
+                  lock.scheduleUnlock {
+                    initialSchedulingContinuation?.resume(Unit)
+                      ?: initialSchedulingSuspensionJob.takeUnless(Job::isActive)?.start()
+                    it
+                  }
+                }
+              )
+            }
+          }
+          .windowed(scheduleCount)
+          .single()
+          .awaitAll()
+      assertThat(actors.first()).isSameAs(actors.last())
     }
   }
 }
---
+++ core/src/test/java/br/com/orcinus/orca/core/auth/FlowExtensionsTests.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2024 Orcinus
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see https://www.gnu.org/licenses.
+ */
+
+package br.com.orcinus.orca.core.auth
+
+import app.cash.turbine.TurbineTestContext
+import app.cash.turbine.test
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import kotlin.test.Test
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+
+internal class FlowExtensionsTests {
+  @Test(expected = IllegalArgumentException::class)
+  fun throwsWhenWindowingWithANegativeSize() {
+    flowOf(0, 1).windowed(-1)
+  }
+
+  @Test
+  fun returnsAnEmptyFlowWhenWindowingWithAZeroedCount() {
+    runTest {
+      flowOf(0, 1).windowed(0).test(validate = TurbineTestContext<List<Int>>::awaitComplete)
+    }
+  }
+
+  @Test
+  fun windows() {
+    runTest {
+      flowOf(0, 2, 4, 16, 256).windowed(2).test {
+        assertThat(awaitItem()).isEqualTo(listOf(0, 2))
+        assertThat(awaitItem()).isEqualTo(listOf(4, 16))
+        awaitComplete()
+      }
+    }
+  }
+}
