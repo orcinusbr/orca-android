c036a6bb5d3535744342d8d91407ea577208aa0e
Jean Silva <jeanbarrossilva@outlook.com>
2025-04-05 07:19:11 -0300

    Parse out Java version from catalog

--- build-src/build.gradle.kts
+++ build-src/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2023–2024 Orcinus
+ * Copyright © 2023–2025 Orcinus
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
@@ -13,16 +13,10 @@
  * not, see https://www.gnu.org/licenses.
  */
 
+import kotlin.io.path.listDirectoryEntries
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-/** Key of this TOML-"`key = value`"-formatted [String]. * */
-private val String.tomlKey
-  get() = replace(" ", "").split('=').first()
-
-/** Value of this TOML-"`key = value`"-formatted [String]. * */
-private val String.tomlValue
-  get() = split('=').last().replace('"', ' ').trim()
+import org.tomlj.Toml
 
 plugins {
   alias(libs.plugins.kotlin.jvm) apply false
@@ -34,71 +28,32 @@ plugins {
 allprojects {
   repositories.mavenCentral()
 
-  withJavaVersionString {
-    tasks.withType<KotlinCompile> {
-      compilerOptions.jvmTarget.set(JvmTarget.fromTarget(this@withJavaVersionString))
+  rootDir.parentFile
+    .toPath()
+    .listDirectoryEntries("gradle")
+    .getOrNull(0)
+    ?.resolve("libs.versions.toml")
+    ?.let(Toml::parse)
+    ?.getTable("versions")
+    ?.getString("java")
+    ?.let { javaVersionAsString ->
+      tasks.withType<KotlinCompile> {
+        compilerOptions.jvmTarget.set(JvmTarget.fromTarget(javaVersionAsString))
 
-      java {
-        sourceCompatibility = JavaVersion.toVersion(this@withJavaVersionString)
-        targetCompatibility = JavaVersion.toVersion(this@withJavaVersionString)
+        java {
+          val version = JavaVersion.toVersion(javaVersionAsString)
+          sourceCompatibility = version
+          targetCompatibility = version
+        }
       }
     }
-  }
 }
+
+buildscript { dependencies.classpath(libs.tomlJ) }
 
 gradlePlugin {
   plugins.register(name) {
     id = name
     implementationClass = "br.com.orcinus.orca.plugin.BuildSrcPlugin"
-  }
-}
-
-/**
- * Extracts the Java version [String] from the `libs.versions.toml` [File] and runs the [action]
- * with it.
- *
- * @param action Operation to be performed with the extracted version.
- */
-fun withJavaVersionString(action: String.() -> Unit) {
-  rootDir.parentFile
-    .listFiles { file, name -> file.isDirectory && name == "gradle" }
-    ?.first()
-    ?.listFiles { _, name -> name == "libs.versions.toml" }
-    ?.first()
-    ?.reader()
-    ?.useLines { lines ->
-      lines
-        .dropUntil { it.isTomlTableHeader("versions") }
-        .first { it.tomlKey == "java" }
-        .tomlValue
-        .run(action)
-    }
-}
-
-/**
- * Drops all elements until the [predicate] is satisfied.
- *
- * @param predicate Condition to be met for a given element to not be dropped.
- */
-fun <T> Sequence<T>.dropUntil(predicate: (T) -> Boolean): Sequence<T> {
-  var toDrop = 0
-  filter { element ->
-    predicate(element).also { isMatch ->
-      if (!isMatch) {
-        ++toDrop
-      }
-    }
-  }
-  return drop(toDrop)
-}
-
-/**
- * Returns whether this [String] is a [TOML table header](https://toml.io/en/v1.0.0#table).
- *
- * @param tableName Name of the table to be checked if it's defined in the header.
- */
-fun String.isTomlTableHeader(tableName: String): Boolean {
-  return with(trim()) {
-    startsWith('[') && endsWith(']') && substringAfter('[').substringBefore(']').trim() == tableName
   }
 }
